6e1a49e2bb2deba791e2628a288c6f5708e0bc1b
Esteban JUNCOSA <Esteban.JUNCOSA@etu.uca.fr>
2023-12-12 10:12:08 +0100

    ajout du déplacement des ennemis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from os import path
+from random import randint,choice
+from math import sqrt
 
 pygame.init()
 length = 1024       # length correspond à la longueur de l'écran, qui est carré
@@ -36,6 +38,17 @@ for file in [path.join(enemy_folder,'right'),path.join(enemy_folder,'left')] :
 
 clock = pygame.time.Clock()         # on définit l'horloge interne
 
+game_on = True
+cpt_mouv = 0                # cpt_mouv compte le temps qui s'écoule entre 2 mouvements, pour que le sprite ne change pas trop vite
+cpt_spawn = 0
+all_enemy = pygame.sprite.Group()
+nb_enemy = 0                # nb_enemy compte le nombre d'ennemis
+max_enemy = 10              # max_enemy correspond au nombre maximum d'ennemis
+cpt_enemy = 0               # cpt_enemy compte le temps écoulé entre 2 apparitions d'ennemis
+delay_enemy = FPS           # delay_enemy définis le temps qu'il faut attendre entre 2 apparitions d'ennemis
+hero_speed = 5              # hero_speed et enemy_speed correspondent à la vitesse du héro et des enemy
+enemy_speed = 3
+
 class Hero(pygame.sprite.Sprite) :
     
     def __init__(self) :
@@ -61,16 +74,38 @@ class Enemy(pygame.sprite.Sprite) :
         self.step = 0                                       # pareil que hero
         self.image = enemies[self.cote][self.step]
         self.rect = self.image.get_rect()
-        self.rect.center = (length-20,length//2)
+        a = randint(-length,length)
+        b = choice([i for i in range(-length,1)]+[i for i in range(length,length*2+1)])
+        if choice([True,False]) :
+            self.rect.center = (a,b)
+        else :
+            self.rect.center = (b,a)
+    
+    def draw(self) :
+        screen.blit(self.image,self.rect)                   # draw sert à dessiner sur l'écran le sprite
+    
+    def update(self) :
+        if self.rect.x not in [(length//2)-40,(length//2)-15] and self.rect.y not in [(length//2)-40,(length//2)-15] :
+            velocity = enemy_speed/sqrt(2)
+        else :
+            velocity = enemy_speed
+        if self.rect.x >= (length//2) - 15 :
+            self.rect.x -= velocity
+        elif self.rect.x <= (length//2) - 40 :
+            self.rect.x += velocity
+        if self.rect.y >= (length//2) - 15 :
+            self.rect.y -= velocity
+        elif self.rect.y <= (length//2) - 40 :
+            self.rect.y += velocity
+        
 
 hero = Hero()
 enemy = Enemy()
-game_on = True
-cpt_mouv = 0                # cpt_mouv compte le temps qui s'écoule entre 2 mouvements, pour que le sprite ne change pas trop vite
 
 while game_on :
     
     cpt_mouv += 1
+    cpt_enemy += 1
     
     clock.tick(FPS)
     bg_change_x = 0         # quand le héro se "déplace", c'est le bg qui en réalité change. On définit bg_change x et y que
@@ -81,19 +116,34 @@ while game_on :
             game_on = False
 
     keys = pygame.key.get_pressed()
+    if (keys[pygame.K_z] or keys[pygame.K_s]) and (keys[pygame.K_q] or keys[pygame.K_d]) :
+        velocity = hero_speed/sqrt(2)
+    else :
+        velocity = hero_speed
     if keys[pygame.K_z] :
-        bg_change_y += 5
+        bg_change_y += velocity
     if keys[pygame.K_q] :
-        bg_change_x += 5
+        bg_change_x += velocity
         if hero.cote == 0 :
             hero.cote = 1
     if keys[pygame.K_s] :                   # on regarde si le joueur appuie sur z,q,s ou d, puis on modifie bg_change
-        bg_change_y -= 5
+        bg_change_y -= velocity
     if keys[pygame.K_d] :
-        bg_change_x -= 5
+        bg_change_x -= velocity
         if hero.cote == 1 :
             hero.cote = 0
+    
+    for enemy in all_enemy.sprites() :
+        enemy.rect.x += bg_change_x
+        enemy.rect.y += bg_change_y
 
+    if cpt_enemy >= delay_enemy and nb_enemy <= max_enemy :
+        all_enemy.add(Enemy())
+        cpt_enemy = 0
+        nb_enemy += 1
+    
+    all_enemy.update()
+    
     if (bg_change_x != 0 or bg_change_y != 0):
         if cpt_mouv >= 10 :
             hero.marcher()                  # s'il y a eu un déplacement et si le chrono le permet, on change le sprite
@@ -116,9 +166,8 @@ while game_on :
         screen.blit(bg,bgs_coords[i])           # on affiche les backgrounds
     
     hero.draw()                                 # on affiche le héro à l'écran
-    screen.blit(enemy.image,enemy.rect)         
-
-
+    all_enemy.draw(screen)
+    
     pygame.display.flip()                       # on retourne l'écran pour afficher les changements
 
 pygame.quit()
