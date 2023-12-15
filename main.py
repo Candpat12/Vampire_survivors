98c66d64a4e967480621ca94aefa030497dd0e66
Esteban JUNCOSA <Esteban.JUNCOSA@etu.uca.fr>
2023-12-15 12:36:29 +0100

    ajout des balles

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ game_folder = path.dirname(__file__)
 sprite_folder = path.join(game_folder,'sprites')
 hero_folder = path.join(sprite_folder,'hero')           # on se place dans les différents dossiers dont on va avoir besoin
 enemy_folder = path.join(sprite_folder,'enemy')
+bullet_folder = path.join(sprite_folder,'bullet')
 
 bg = pygame.image.load(path.join(sprite_folder, "bg.png")).convert()        # on importe le sprite du fond d'écran
 bgs_coords = [[-length,-length],[-length,0],[-length,length],[0,-length],[0,0]\
@@ -35,6 +36,10 @@ for file in [path.join(enemy_folder,'right'),path.join(enemy_folder,'left')] :
         a = pygame.transform.scale2x(a)
         l.append(a)
     enemies.append(l)
+    
+bullets = []
+for i in range(1,5) :
+    bullets.append(pygame.image.load((path.join(bullet_folder,"bullet_"+str(i)+'.png'))))
 
 clock = pygame.time.Clock()         # on définit l'horloge interne
 
@@ -43,13 +48,16 @@ cpt_mouv = 0                # cpt_mouv compte le temps qui s'écoule entre 2 mou
 cpt_spawn = 0
 cpt_immunity = FPS*2
 all_enemy = pygame.sprite.Group()
+all_bullets = pygame.sprite.Group()
 nb_enemy = 0                # nb_enemy compte le nombre d'ennemis
 max_enemy = 100              # max_enemy correspond au nombre maximum d'ennemis
 cpt_enemy = 0               # cpt_enemy compte le temps écoulé entre 2 apparitions d'ennemis
-delay_enemy = FPS*0.25           # delay_enemy définis le temps qu'il faut attendre entre 2 apparitions d'ennemis
+delay_enemy = FPS*0.5           # delay_enemy définis le temps qu'il faut attendre entre 2 apparitions d'ennemis
+cpt_bullet = 0
+delay_bullet = FPS // 2
 hero_speed = 5              # hero_speed et enemy_speed correspondent à la vitesse du héro et des enemy
 enemy_speed = 3
-max_health = 100
+max_health = 100            # max_health et hp correspondent respectivement aux PV maximums et aux PV actuels
 hp = 100
 
 class Hero(pygame.sprite.Sprite) :
@@ -125,13 +133,35 @@ class Enemy(pygame.sprite.Sprite) :
             self.rect.y -= self.liste_mouv[1]*2
             if cpt_immunity > FPS*0.5 :
                 self.knocked = False
-            
+
+class Bullet(pygame.sprite.Sprite) :
+    
+    def __init__(self,cote) :
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullets[0]
+        self.status = 0
+        self.rect = self.image.get_rect()
+        self.rect.center = (length//2,length//2)
+        self.cpt_rota = 0
+        self.delay_rota = FPS//16
+        if cote == 0 :
+            self.mouv = 7
+        else :
+            self.mouv = -7
+        
+    def update(self) :
+        global all_enemy
+        self.rect.x += self.mouv
+        self.cpt_rota += 1
+        if self.cpt_rota >= self.delay_rota :
+            self.status = (self.status+1) % 4
+            self.image = bullets[self.status]
+            self.cpt_rota = 0
             
             
     
 
 hero = Hero()
-enemy = Enemy()
 
 #--------------------Programme principal--------------------
 
@@ -142,8 +172,11 @@ while game_on :
         
     cpt_mouv += 1           # on incrémente chaque compteur
     cpt_enemy += 1
+    cpt_bullet += 1
+    
     if cpt_immunity < FPS*2 :
         cpt_immunity += 1
+        
     
     clock.tick(FPS)
     
@@ -159,7 +192,7 @@ while game_on :
             game_on = False
 
     keys = pygame.key.get_pressed()
-    if (keys[pygame.K_z] or keys[pygame.K_s]) and (keys[pygame.K_q] or keys[pygame.K_d]) :
+    if int(keys[pygame.K_z]) + int(keys[pygame.K_s]) + int(keys[pygame.K_q]) + int(keys[pygame.K_d]) == 2 :
         velocity = hero_speed/sqrt(2)               # la vitesse change selon si l'on est en diagonale ou en ligne droite
     else :                                          # on se sert du théorème de pythagore pour que la vitesse reste la même
         velocity = hero_speed
@@ -181,14 +214,23 @@ while game_on :
     for enemy in all_enemy.sprites() :
         enemy.rect.x += bg_change_x             # ici, on éloigne (ou rapproche) le joueur de l'ennemi selon son déplacement
         enemy.rect.y += bg_change_y
-
+    
+    for bullet in all_bullets.sprites() :
+        bullet.rect.x += bg_change_x             # ici, on éloigne (ou rapproche) le joueur de l'ennemi selon son déplacement
+        bullet.rect.y += bg_change_y
 
     if cpt_enemy >= delay_enemy and nb_enemy <= max_enemy :
         all_enemy.add(Enemy())
         cpt_enemy = 0                           # on définit les conditions qui font qu'un nouvel ennemi peut apparaître
         nb_enemy += 1
     
-    all_enemy.update()                          # chaque ennemi se rapproche du joueurq
+    if cpt_bullet >= delay_bullet :
+        a = Bullet(hero.cote)
+        all_bullets.add(a)
+        cpt_bullet = 0
+    
+    all_enemy.update()                          # chaque ennemi se rapproche du joueur
+    all_bullets.update()
     
     if (bg_change_x != 0 or bg_change_y != 0):
         if cpt_mouv >= 10 :
@@ -199,8 +241,14 @@ while game_on :
         hero.image = heros[hero.cote][hero.step]
             
     
-
-
+#--------------------condition de mort des divers sprites--------------------
+    for bullet in all_bullets.sprites() :
+        if bullet.rect.x > length*2 or bullet.rect.x < -length or bullet.rect.y > length*2  or bullet.rect.y < -length :
+            all_bullets.remove(bullet)
+        for enemy in all_enemy.sprites() :
+            if pygame.sprite.collide_rect(enemy,bullet) :
+                all_bullets.remove(bullet)
+                all_enemy.remove(enemy)
 
 
 
@@ -224,6 +272,7 @@ while game_on :
     
     hero.draw()                                 # on affiche le héro et les ennemis à l'écran
     all_enemy.draw(screen)
+    all_bullets.draw(screen)
     pygame.draw.rect(screen,(255,0,0),(476,558,72,10))
     pygame.draw.rect(screen,(0,255,0),(476,558,72*(hp/max_health),10))
     pygame.display.flip()                       # on retourne l'écran pour afficher les changements
